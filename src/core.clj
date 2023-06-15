8d87ea037301ca06acca0d5dcbae3c7e275702be
Vincent Liao <vincent.liao@protonmail.com>
2023-06-15 09:28:51 +0700

    add: users first week cast map

--- src/core.clj
+++ src/core.clj
@@ -3,9 +3,8 @@
             [clj-time.core :as t]
             [writer :as w]))
 
-;; TODO: use the actual cast data later on, 1000 for speeding up iteration
 (def users (w/read-data "data/users.edn"))
-(def casts (w/read-data "data/casts1000.edn"))
+(def casts (w/read-data "data/casts.edn"))
 
 (defmulti get-timestamp-joda (fn [data]
                                (cond
@@ -18,7 +17,6 @@
   (c/from-long (:casts/timestamp cast)))
 (defmethod get-timestamp-joda :default [data] data)
 
-;; use this for all map-maker
 (def start-timestamp (->> users
                           (sort-by :users/registered_at)
                           first
@@ -41,25 +39,31 @@
         (.put counter week (conj (.getOrDefault counter week []) user))))
     (into {} counter)))
 
-(defn get-casts-by-user [casts user]
-  (filter (fn [cast] (= (:casts/author_fid cast) (:users/fid user))) casts))
-
-(defn get-casts-in-week [casts user]
-  (let [one-week-later (t/plus (get-timestamp-joda user) (t/weeks 1))]
-    (filter (fn [cast]
-              (and (t/after? (get-timestamp-joda cast) (get-timestamp-joda user))
-                   (t/before? (get-timestamp-joda cast) one-week-later)))
-            casts)))
-
 (defn get-weekly-casts [casts users]
-  (reduce (fn [result user]
-            (assoc result (:users/username user) (-> casts
-                                                     (get-casts-by-user user)
-                                                     (get-casts-in-week user)
-                                                     count)))
-          {}
-          users))
+  (let [get-casts-by-user (fn [casts user]
+                            (filter (fn [cast] (= (:casts/author_fid cast) (:users/fid user))) casts))
+        get-casts-in-week (fn [casts user]
+                            (let [one-week-later (t/plus (get-timestamp-joda user) (t/weeks 1))]
+                              (filter (fn [cast]
+                                        (and (t/after? (get-timestamp-joda cast) (get-timestamp-joda user))
+                                             (t/before? (get-timestamp-joda cast) one-week-later)))
+                                      casts)))]
+    (reduce (fn [result user]
+              (assoc result (:users/username user)
+                     (-> casts
+                         (get-casts-by-user user)
+                         (get-casts-in-week user)
+                         count)))
+            {}
+            users)))
 
-(def registrations-map (make-registrations-map users start-timestamp))
-(def user-batch (:registrations/week-99 registrations-map))
-(def weekly-casts (get-weekly-casts casts user-batch))
+(def registrations-map (w/read-data "data/registrations-map.edn"))
+(defn get-weekly-casts-for-all-weeks [casts registrations-map]
+  (reduce (fn [result week-key]
+            (let [user-batch (get registrations-map week-key)
+                  weekly-casts-for-week (get-weekly-casts casts user-batch)]
+              (assoc result week-key weekly-casts-for-week)))
+          {}
+          (keys registrations-map)))
+
+(def weekly-casts-map (get-weekly-casts-for-all-weeks casts registrations-map))
